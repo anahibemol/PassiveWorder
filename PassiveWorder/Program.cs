beb3a07b6430aa43f64206ceaa936ecc9a9f0f54
NuloSois <104397117+NuloSois@users.noreply.github.com>
2022-05-01 22:07:51 -0300

    Add files via upload

---
+++ PassiveWorder/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PassiveWorder
+{
+    class Passworder {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write the password");
+            
+            string? Password = "";
+            
+            Console.WriteLine(Password);
+            Password = Console.ReadLine();
+
+            if(Password == "1234")
+            {
+                Console.WriteLine("Permission Granted");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Password");
+            }
+        }
+    }
+}
